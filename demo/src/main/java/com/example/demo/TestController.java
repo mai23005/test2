4554fec5d294b3918da1d20a393ba2bc8023c1d9
mai23005 <149181586+mai23005@users.noreply.github.com>
2023-12-22 17:56:09 +0000

    Init commit

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String name) {
+        return "Hello " + name; 
+    }
+}
